f76f5c253a24c7fb3d912751c55228bcd6952a69
Lempereurjulien <lempereur.julien83@gmail.com>
2021-09-15 17:35:35 +0200

    Ajout fonction comptable dans fct.inc.php/class.pdogsb.inc.php/c_connexion.php

--- includes/fct.inc.php
+++ includes/fct.inc.php
@@ -39,7 +39,11 @@ function connecter($idVisiteur, $nom, $prenom)
     $_SESSION['nom'] = $nom;
     $_SESSION['prenom'] = $prenom;
 }
-
+function connecter_comptable($idComptable,$nom,$prenom){
+    $_SESSION['idComptable']=$idComptable;
+    $_SESSION['nom']=$nom;
+    $_SESSION['prenom']=$prenom;
+}
 /**
  * Détruit la session active
  *
